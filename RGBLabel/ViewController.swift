fd10513137966fb612e8b9789bbdd197848c89ee
Evgeniya Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-03-04 23:41:39 +0300

    logic between color labels and values of sliders

--- RGBLabel/ViewController.swift
+++ RGBLabel/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+// MARK: outlets
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 20
+        
+        redLabel.text = String(round(redSlider.value * 100) / 100)
+        greenLabel.text = String(round(greenSlider.value * 100) / 100)
+        blueLabel.text = String(round(blueSlider.value * 100) / 100)
     }
 
-
+    @IBAction func slidersAction(_ sender: UISlider) {
+        redLabel.text = String(round(redSlider.value * 100) / 100)
+        greenLabel.text = String(round(greenSlider.value * 100) / 100)
+        blueLabel.text = String(round(blueSlider.value * 100) / 100)
+    }
 }
 
