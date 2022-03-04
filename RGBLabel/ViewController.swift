b390989e7b926b12c14877dd338e65678271f689
Evgeniya Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-03-04 23:52:25 +0300

    logic between sliders and color view

--- RGBLabel/ViewController.swift
+++ RGBLabel/ViewController.swift
@@ -29,12 +29,25 @@ class ViewController: UIViewController {
         redLabel.text = String(round(redSlider.value * 100) / 100)
         greenLabel.text = String(round(greenSlider.value * 100) / 100)
         blueLabel.text = String(round(blueSlider.value * 100) / 100)
+        
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+           
     }
 
     @IBAction func slidersAction(_ sender: UISlider) {
         redLabel.text = String(round(redSlider.value * 100) / 100)
         greenLabel.text = String(round(greenSlider.value * 100) / 100)
         blueLabel.text = String(round(blueSlider.value * 100) / 100)
+        
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
     }
 }
 
